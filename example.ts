7fb8453a3d8eab2d6de1b736627cdf8ecbedc44f
Neil Dobson <neil.dobson71@gmail.com>
2021-08-01 20:01:32 +1000

    Added Typescript and example

---
+++ example.ts
@@ -0,0 +1,15 @@
+type User = {
+    name: string;
+    age: number;
+};
+
+function isAdult(user: User): boolean {
+    return user.age >= 18;
+}
+
+const justine: User = {
+    name: 'Justine',
+    age: 18,
+};
+
+const isJustineAnAdult: boolean = isAdult(justine);
